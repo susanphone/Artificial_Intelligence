56f5b31a9e0a2776da7b0e4a87b07ae319080a97
hbmadsen <>
2021-09-16 16:41:39 -0600

    random board generation

--- Projects/Project1/sudoku_solver/AgentTemp.java
+++ Projects/Project1/sudoku_solver/AgentTemp.java
@@ -1,4 +1,5 @@
 package sudoku_solver;
+import java.util.Random;
 
 public class AgentTemp {
     //local search using a genetic algorithm with a penalty function and tournament selection
@@ -6,6 +7,27 @@ public class AgentTemp {
         //generate 20 boards to create initial population
         //population is stored in an array of Boards
         Board[] population = new Board[20];
+        Memory memory = new Memory();
+        Random rand = new Random();
+        //fill the empty spaces with a random value to create the initial population of boards
+        for(int k = 0; k < 20; k++){
+            Board tempBoard = new Board();
+            for(int i = 0; i < 9; i++){
+                for(int j = 0; j < 9; j++){
+                    if(memory.board.board[i][j] == 0){
+                        int n = rand.nextInt(9) + 1;// add 1 to make the range 1-9
+                        tempBoard.board[i][j] = n;
+                    }else{
+                        tempBoard.board[i][j] = memory.board.board[i][j];
+                    }
+                }
+            }
+            population[k] = tempBoard;
+            Board.printBoard(tempBoard.board);
+        }
+
+
+
 
         //check to see if solution exists
         //select the two best boards (based off of fitness function) and reproduce
