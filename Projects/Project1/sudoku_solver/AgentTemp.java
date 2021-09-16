1176f854b6197f2cca2e17ce20a686cf2c5a87b6
hbmadsen <>
2021-09-16 13:32:26 -0600

    commented layout

--- Projects/Project1/sudoku_solver/AgentTemp.java
+++ Projects/Project1/sudoku_solver/AgentTemp.java
@@ -2,6 +2,10 @@ package sudoku_solver;
 
 public class AgentTemp {
     public static void genetic() {
+        //generate 20 boards to create initial population
+        //check to see if solution exists
+        //select the two best boards (based off of fitness function) and reproduce
+        //repeat process
     }
     
 }
