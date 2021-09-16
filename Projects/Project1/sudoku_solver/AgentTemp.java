8b7252e76c910fe5b1a1ce2192ce2722f30d34f9
hbmadsen <>
2021-09-16 15:16:59 -0600

    updates

--- Projects/Project1/sudoku_solver/AgentTemp.java
+++ Projects/Project1/sudoku_solver/AgentTemp.java
@@ -1,11 +1,17 @@
 package sudoku_solver;
 
 public class AgentTemp {
+    //local search using a genetic algorithm with a penalty function and tournament selection
     public static void genetic() {
         //generate 20 boards to create initial population
+        //population is stored in an array of Boards
+        Board[] population = new Board[20];
+
         //check to see if solution exists
         //select the two best boards (based off of fitness function) and reproduce
         //repeat process
+
+
     }
     
 }
