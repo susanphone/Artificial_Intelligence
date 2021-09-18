082937706aedaf5cdf1f345c0fb3a0be49682829
hbmadsen <>
2021-09-18 12:01:31 -0600

    helper functions class

--- Projects/Project1/sudoku_solver/AgentTemp.java
+++ Projects/Project1/sudoku_solver/AgentTemp.java
@@ -2,105 +2,8 @@ package sudoku_solver;
 import java.util.Random;
 
 public class AgentTemp {
-    public static float  fitness(Board board){
-
-        //find number of correct rows and divide by total rows
-        //find number of correct columns and divide by total columns
-        //find number of correct matrices and divide by total matrices
-        // add up all three values and divide by 3
 
 
-        int[] vals = new int[9];
-
-        float valid_rows = 9;
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                if(vals[board.board[i][j]-1] == 0){
-                    vals[board.board[i][j]-1] = 1;
-                }
-                else valid_rows -= 1;
-            }
-        }
-        float row_val = valid_rows / 9;
-
-        float valid_cols = 9;
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                if(vals[board.board[j][i]-1] == 0){
-                    vals[board.board[j][i]-1] = 1;
-                }
-                else valid_cols -= 1;
-            }
-        }
-        float col_val = valid_cols / 9;
-
-        float valid_matrices = 0;
-        int[] vals1;
-        int[] vals2;
-        int[] vals3;
-        for(int num = 0; num < 9; num+=3){
-            //Resets the checkers
-            vals1 = new int[9];
-            vals2 = new int[9];
-            vals3 = new int[9];
-
-            for(int i = num; i < num + 3; i++)
-            {
-                //Checks matrices on left side
-                for(int j = 0; j < 3; j++)
-                {
-                    if(board.board[i][j] != 0){
-                        if(vals1[board.board[i][j]-1] == 0){
-                            vals1[board.board[i][j]-1] = 1;
-                        }
-                        else valid_matrices -= 1;
-                    }
-                }
-
-                //Checks matrices in the middle
-                for(int j = 3; j < 6; j++)
-                {
-                    if(board.board[i][j] != 0){
-                        if(vals2[board.board[i][j]-1] == 0){
-                            vals2[board.board[i][j]-1] = 1;
-                        }
-                        else valid_matrices -= 1;
-                    }
-                }
-
-                //Checks matrices on right side
-                for(int j = 6; j < 9; j++)
-                {
-                    if(board.board[i][j] != 0){
-                        if(vals3[board.board[i][j]-1] == 0){
-                            vals3[board.board[i][j]-1] = 1;
-                        }
-                        else valid_matrices -= 1;
-                    }
-                }
-            }
-        }
-        float matrix_val = valid_matrices / 9;
-
-        float fitness_val = (row_val + col_val + matrix_val) / 3;
-        return fitness_val;
-
-    }
-    public static int[] removeArrayElem(int[] boards, int index){
-        int[] tempArray = new int[boards.length - 1];
-
-        if(boards == null || index < 0 || index >= boards.length){
-            return  boards;
-        }
-        for(int i = 0, j = 0; i < boards.length; i ++){
-            if(i == index){
-                continue;
-            }else{
-                tempArray[j++] = boards[i];
-            }
-        }
-        return tempArray;
-    }
     //local search using a genetic algorithm with a penalty function and tournament selection
     public static void genetic() {
         //generate 20 boards to create initial population
@@ -137,12 +40,40 @@ public class AgentTemp {
         int iteration = 1000; //limit number of iterations
 
         do{
+            Board[] new_population = new Board[population.length];
+            //select two random candidates using tournament selection
+            Board candidate1 = new Board();
+            Board candidate2 = new Board();
+            Board parent1 = new Board();
+            Board parent2 = new Board();
+            Board[] temp_population = new Board[population.length];
+            //create two tournaments and select 'winners' from each to be parents
 
-            //select two random candidates
-            //calculate the fitness of each candidate
-            //repeat
-            //use the two candidates with the highest fitness from each 'tournament'  to be parents
+            for( int l = 0; l < 2; l++){
+                int n1 = rand.nextInt(population.length);
+                candidate1 = population[n1];
+                temp_population = HelperFunctions.removeArrayElem(population, n1);
+                int n2 = rand.nextInt(temp_population.length);
+                candidate2 = temp_population[n2];
+
+                //calculate the fitness of each candidate
+                float fitness1 = HelperFunctions.fitness(candidate1);
+                float fitness2 = HelperFunctions.fitness(candidate2);
+
+                //use the two candidates with the highest fitness from each 'tournament'  to be parents
+                if(fitness1 > fitness2){
+                    if(l ==0){
+                        parent1 = candidate1;
+                    }else parent2 = candidate1;
+                }else {
+                    if(l == 0){
+                        parent1 = candidate2;
+                    }else parent2 = candidate2;
+                }
+            }
+
             //crossover and mutate to make a new population
+
             //repeat entire process until a new 'generation' is reached and start over
             //finish when solution is found (fitness == 1) or number of iterations is reached
 
