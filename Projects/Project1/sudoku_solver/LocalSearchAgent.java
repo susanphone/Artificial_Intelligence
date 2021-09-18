bf38bcd05920bf8079bae380a86fea9b65d12864
Susan McCartney <57429748+susanphone@users.noreply.github.com>
2021-09-18 13:26:45 -0600

    almost working simulated annealing

--- Projects/Project1/sudoku_solver/LocalSearchAgent.java
+++ Projects/Project1/sudoku_solver/LocalSearchAgent.java
@@ -3,19 +3,16 @@ package sudoku_solver;
 import java.io.File;
 import java.util.Random;
 public class LocalSearchAgent {
-    private int row;
     int initialValues[] = new int[1]; // whatever value is not a 0 in the initial board
-    private int column;
     Memory memory = new Memory();
 
     public void LocalSearchAgent(int row, int column, int memory[][]) {
-        this.row = row;
-        this.column = column;
+        this.memory.board.board = memory;
     }
 
     public static void simulatedAnnealing() {
-        Random rand =  new Random();
-        int[] possibleValues = new int[] {
+        Random rand = new Random();
+        int[] possibleValues = new int[]{
                 1, 1, 1, 1, 1, 1, 1, 1, 1,
                 2, 2, 2, 2, 2, 2, 2, 2, 2,
                 3, 3, 3, 3, 3, 3, 3, 3, 3,
@@ -31,68 +28,69 @@ public class LocalSearchAgent {
         Board.printBoard(memory.board.board);
 
         int position[][] = memory.board.board;
-        int initialValue = 0;
-        int remainingValues = 81 - initialValue;
+        int initialValue[] = new int[1];
+        int remainingValues = 81 - initialValue[1];
         //Loops through i values
         for (int i = 0; i < 9; i++) {
             //Loops through j values
             for (int j = 0; j < 9; j++) {
-                if (initialValue != 0) {
-                    HelperFunctions.removeArrayElem(possibleValues[initialValue]);
+                if (initialValue[j] != 0) {
+                    HelperFunctions.removeArrayElem(possibleValues[i][j]);
                     // remove intial Values from possibleValues
 
-        } else {
-            // put remaining values into the spots whose value is 0, randomly
-            int nRow = rand.nextInt(9) + 1;// add 1 to make the range 1-9
-            int mColumn = rand.nextInt(9)+1;
+                } else {
+                    // put remaining values into the spots whose value is 0, randomly
+                    int nRow = rand.nextInt(9) + 1;// add 1 to make the range 1-9
+                    int mColumn = rand.nextInt(9) + 1;
 
-            // pick a random spot with 0
-            int randomPos[][] = new int[nRow][mColumn];
-            // and empty the array
-            HelperFunctions.removeArrayElem(randomPos);
-            HelperFunctions.removeArrayElem(randomPos);
-        }
-
-    }
-    // COst function used to compare it switching the values would reduce the cost on the board.
-    // Goal is to have the cost be 0.
-    public int costFunction(int memory[][]) {
-        int columnCost = 0;
-        int rowCost = 0;
-        int currentValue = 0;
-        int neighborValue = 0;
-        int currentTotalErrors =0;
-        // check for duplicates in rows
-        for (int row = 1; row < 10; row++) {
-            for (int column = 1; column < 10; column++) {
-                // if the value in the current spot is the same as the value as a neighbor
-                // or the current value is equal to zero
-                if (currentValue == neighborValue || currentValue == 0) {
-                    columnCost += 1;
+                    // pick a random spot with 0
+                    int randomPos[][] = new int[nRow][mColumn];
+                    // and empty the array
+                    HelperFunctions.removeArrayElem(possibleValues[this.row][this.column]);
                 }
+
             }
         }
-        // check for duplicates in columns
-        for (column = 1; column < 10; column++) {
-            for (row = 1; row < 10; row++) {
-                if (currentValue == neighborValue || currentValue == 0) {
-                    for (int num = 1; num <= 9; num++) {
-                        memory[row][column] = num; //Sets the space at the current index (i,j) to num
-                        rowCost += 1;
+    }
+            // Cost function used to compare it switching the values would reduce the cost on the board.
+            // Goal is to have the cost be 0.
+            public int costFunction ( int memory[][]){
+                int columnCost = 0;
+                int rowCost = 0;
+                int currentValue = 0;
+                int neighborValue = 0;
+                int currentTotalErrors = 0;
+                // check for duplicates in rows
+                for (int row = 1; row < 10; row++) {
+                    for (int column = 1; column < 10; column++) {
+                        // if the value in the current spot is the same as the value as a neighbor
+                        // or the current value is equal to zero
+                        if (currentValue == neighborValue || currentValue == 0) {
+                            columnCost += 1;
+                        }
                     }
                 }
-            }
+                // check for duplicates in columns
+                for (int column = 1; column < 10; column++) {
+                    for (int row = 1; row < 10; row++) {
+                        if (currentValue == neighborValue || currentValue == 0) {
+                            for (int num = 1; num <= 9; num++) {
+//                                memory[row][column] = num; //Sets the space at the current index (i,j) to num
+                                rowCost += 1;
+                            }
+                        }
+                    }
 
-            currentTotalErrors = rowCost + columnCost;
-            int possibleTotalErrors = 81;
+                    currentTotalErrors = rowCost + columnCost;
+                    int possibleTotalErrors = 81;
 
-            // if switching the values made the cost higher, then switch them back
-            if (possibleTotalErrors > currentTotalErrors) {
+                    // if switching the values made the cost higher, then switch them back
+                    if (possibleTotalErrors > currentTotalErrors) {
 
-            } else {
-                //update the board
+                    } else {
+                        //update the board
+                    }
+                }
+                return currentTotalErrors;
             }
         }
-        return currentTotalErrors;
-    }
-}
