01d369625141e60c6e60b24efad5683540962734
hbmadsen <>
2021-09-19 17:28:43 -0600

    more progress

--- Projects/Project1/sudoku_solver/AgentTemp.java
+++ Projects/Project1/sudoku_solver/AgentTemp.java
@@ -10,33 +10,20 @@ public class AgentTemp {
         Board solutionBoard = new Board();
         Memory memory = new Memory();
         Random rand = new Random();
-        //fill the empty spaces with a random value to create the initial population of boards
-        for(int k = 0; k < 20; k++){
-            Board tempBoard = new Board();
-            for(int i = 0; i < 9; i++){
-                for(int j = 0; j < 9; j++){
-                    if(memory.board.board[i][j] == 0){
-                        int n = rand.nextInt(9) + 1;// add 1 to make the range 1-9
-                        tempBoard.board[i][j] = n;
-                    }else{
-                        tempBoard.board[i][j] = memory.board.board[i][j];
-                    }
-                }
-            }
-            population[k] = tempBoard;
-            Board.printBoard(tempBoard.board);
-        }
+
+        population = HelperFunctions.gernerateBoards(population);
+
         //check to see if solution exists
         boolean solutionFound = false;
-        for(int b = 0; b < 20; b++){
-            if(Checker.checkRows(population[b].board) && Checker.checkCols(population[b].board) && Checker.checkMatrices(population[b].board)){
+        for(int b = 0; b < 20; b++) {
+            if (Checker.checkRows(population[b].board) && Checker.checkCols(population[b].board) && Checker.checkMatrices(population[b].board)) {
                 Board.printBoard(population[b].board);
                 solutionFound = true;
                 break;
             }
         }
-
-        int iteration = 1000; //limit number of iterations
+        //int iteration = 2; //limit number of iterations
+        int iteration = 0;
 
         do{
 
@@ -44,17 +31,17 @@ public class AgentTemp {
             int new_gen_pop_size = 0;
             while(new_gen_pop_size < new_population.length){
                 //select two random candidates using tournament selection
-                Board candidate1 = new Board();
-                Board candidate2 = new Board();
+                Board candidate1;
+                Board candidate2;
                 Board parent1 = new Board();
                 Board parent2 = new Board();
-                Board[] temp_population = new Board[population.length];
+                Board[] temp_population;
                 //HelperFunctions.tournamentSelection(population, candidate1, candidate2, parent1, parent2);
 
                 for( int l = 0; l < 2; l++){
                     int n1 = rand.nextInt(population.length);
                     candidate1 = population[n1];
-                    temp_population = HelperFunctions.removeArrayElem(population, n1);
+                    temp_population = HelperFunctions.removeBoardArrayElem(population, n1);
                     int n2 = rand.nextInt(temp_population.length);
                     candidate2 = temp_population[n2];
 
@@ -62,7 +49,21 @@ public class AgentTemp {
                     float fitness1 = HelperFunctions.fitness(candidate1);
                     float fitness2 = HelperFunctions.fitness(candidate2);
 
-                    if(fitness1 == 1)
+                    //Board.printBoard(candidate1.board);
+                    System.out.println("Fitness 1: " + fitness1 + "\n");
+                    //Board.printBoard(candidate1.board);
+                    System.out.println("Fitness 2: " + fitness2 + "\n");
+
+                    if(fitness1 == 1){
+                        Board.printBoard(candidate1.board);
+                        solutionFound = true;
+                        solutionBoard = candidate1;
+                    }
+                    if(fitness2 == 1){
+                        Board.printBoard(candidate2.board);
+                        solutionFound = true;
+                        solutionBoard = candidate2;
+                    }
                     //use the two candidates with the highest fitness from each 'tournament'  to be parents
                     if(fitness1 > fitness2){
                         if(l ==0){
@@ -77,20 +78,24 @@ public class AgentTemp {
                 //crossover - one point (spot 60)
                 Board tempBoard1 = new Board();
                 Board tempBoard2 = new Board();
+                boolean cross_point = false;
                 for(int i = 0; i < 9; i++) {
                     for (int j = 0; j < 9; j++) {
-                        if (memory.board.board[i][j] == 0 && i * j > 60) {
+                        if(i == 6 && j == 5){
+                            cross_point = true;
+                        }
+                        if (memory.board.board[i][j] == 0 && cross_point) {
                             tempBoard1.board[i][j] = parent2.board[i][j];
                             tempBoard2.board[i][j] = parent1.board[i][j];
                         } else {
                             tempBoard1.board[i][j] = parent1.board[i][j];
-                            tempBoard1.board[i][j] = parent2.board[i][j];
+                            tempBoard2.board[i][j] = parent2.board[i][j];
                         }
                     }
                 }
 
-                //parent1 = tempBoard1;
-                //parent2 = tempBoard2;
+                parent1 = tempBoard1;
+                parent2 = tempBoard2;
                 //random mutation
                 parent1 = HelperFunctions.mutate(parent1);
                 parent2 = HelperFunctions.mutate(parent2);
@@ -98,15 +103,29 @@ public class AgentTemp {
                 new_population[new_gen_pop_size++] = parent1;
                 new_population[new_gen_pop_size++] = parent2;
                 //repeat entire process until a new 'generation' is reached and start over
+                if(new_gen_pop_size == 19){
+                    population = new_population;
+                    new_population = new Board[20];
+                }
                 //finish when solution is found (fitness == 1) or number of iterations is reached
+
+//                if(iteration == 0){
+//                    float parent1_fitness = HelperFunctions.fitness(parent1);
+//                    float parent2_fitness = HelperFunctions.fitness(parent2);
+//
+//                    if(parent1_fitness > parent2_fitness){
+//                        solutionBoard = parent1;
+//                        Board.printBoard(parent1.board);
+//                    }else{
+//                        solutionBoard = parent2;
+//                        Board.printBoard(parent2.board);
+//                    }
+//                }
             }
 
-
-        }while (!solutionFound && iteration > 0);
-
-            //select the two best boards (based off of fitness function) and reproduce
-            //repeat process
-
+            System.out.println("Generation: " + iteration + "\n");
+            iteration++;
+    }while (!solutionFound);
 
     return solutionBoard;
     }
--- Projects/Project1/sudoku_solver/HelperFunctions.java
+++ Projects/Project1/sudoku_solver/HelperFunctions.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class HelperFunctions {
 
-    public static float  fitness(Board board){
+    public static float fitness(Board board){
 
         //find number of correct rows and divide by total rows
         //find number of correct columns and divide by total columns
@@ -12,30 +12,48 @@ public class HelperFunctions {
 
 
         int[] vals = new int[9];
-
+        boolean isValidRow = true;
         float valid_rows = 9;
         for(int i = 0; i < 9; i++){
+            isValidRow = true;
+            vals = new int[9];
             for(int j = 0; j < 9; j++){
                 if(vals[board.board[i][j]-1] == 0){
                     vals[board.board[i][j]-1] = 1;
                 }
-                else valid_rows -= 1;
+                else {
+                    isValidRow = false;
+                    break;
+                }
+            }
+            if(!isValidRow){
+                valid_rows--;
             }
         }
         float row_val = valid_rows / 9;
 
+        boolean isValidCol = true;
         float valid_cols = 9;
         for(int i = 0; i < 9; i++){
+            isValidCol = true;
+            vals = new int[9];
             for(int j = 0; j < 9; j++){
                 if(vals[board.board[j][i]-1] == 0){
                     vals[board.board[j][i]-1] = 1;
                 }
-                else valid_cols -= 1;
+                else{
+                    isValidCol = false;
+                    break;
+                }
+            }
+            if(!isValidCol){
+                valid_cols--;
             }
         }
         float col_val = valid_cols / 9;
 
-        float valid_matrices = 0;
+        float valid_matrices = 9;
+        boolean isValidMatrix = true;
         int[] vals1;
         int[] vals2;
         int[] vals3;
@@ -47,17 +65,16 @@ public class HelperFunctions {
 
             for(int i = num; i < num + 3; i++)
             {
+                isValidMatrix = true;
                 //Checks matrices on left side
-                for(int j = 0; j < 3; j++)
-                {
+                for(int j = 0; j < 3; j++){
                     if(board.board[i][j] != 0){
                         if(vals1[board.board[i][j]-1] == 0){
                             vals1[board.board[i][j]-1] = 1;
                         }
-                        else valid_matrices -= 1;
+                        else isValidMatrix = false;
                     }
                 }
-
                 //Checks matrices in the middle
                 for(int j = 3; j < 6; j++)
                 {
@@ -65,19 +82,21 @@ public class HelperFunctions {
                         if(vals2[board.board[i][j]-1] == 0){
                             vals2[board.board[i][j]-1] = 1;
                         }
-                        else valid_matrices -= 1;
+                        else isValidMatrix = false;
                     }
                 }
 
                 //Checks matrices on right side
-                for(int j = 6; j < 9; j++)
-                {
+                for(int j = 6; j < 9; j++){
                     if(board.board[i][j] != 0){
                         if(vals3[board.board[i][j]-1] == 0){
                             vals3[board.board[i][j]-1] = 1;
                         }
-                        else valid_matrices -= 1;
+                        else isValidMatrix = false;
                     }
+                }
+                if(!isValidMatrix){
+                    valid_matrices--;
                 }
             }
         }
@@ -87,7 +106,23 @@ public class HelperFunctions {
         return fitness_val;
 
     }
-    public static Board[] removeArrayElem(Board[] boards, int index){
+    public static int[] removeIntArrayElem(int[] arr, int index){
+        int[] tempArray = new int[arr.length - 1];
+
+        if(arr == null || index < 0 || index >= arr.length){
+            return  arr;
+        }
+        for(int i = 0, j = 0; i < arr.length; i ++){
+            if(i == index){
+                continue;
+            }else{
+                tempArray[j++] = arr[i];
+            }
+        }
+        return tempArray;
+    }
+
+    public static Board[] removeBoardArrayElem(Board[] boards, int index){
         Board[] tempArray = new Board[boards.length - 1];
 
         if(boards == null || index < 0 || index >= boards.length){
@@ -102,10 +137,59 @@ public class HelperFunctions {
         }
         return tempArray;
     }
-  
-    public static void crossover(Board parent1, Board parent2){
-        Memory memory = new Memory();
 
+    public static int[] generateRemainingValues(Board b){
+        Memory memory = new Memory();
+        int[] possibleValues = new int[]{
+                1, 1, 1, 1, 1, 1, 1, 1, 1,
+                2, 2, 2, 2, 2, 2, 2, 2, 2,
+                3, 3, 3, 3, 3, 3, 3, 3, 3,
+                4, 4, 4, 4, 4, 4, 4, 4, 4,
+                5, 5, 5, 5, 5, 5, 5, 5, 5,
+                6, 6, 6, 6, 6, 6, 6, 6, 6,
+                7, 7, 7, 7, 7, 7, 7, 7, 7,
+                8, 8, 8, 8, 8, 8, 8, 8, 8,
+                9, 9, 9, 9, 9, 9, 9, 9, 9};
+
+        for(int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (memory.board.board[i][j] != 0) {
+                    for (int val = 0; val < possibleValues.length; val++) {
+                        if (possibleValues[val] == memory.board.board[i][j]) {
+                            possibleValues = removeIntArrayElem(possibleValues, val);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return possibleValues;
+    }
+  
+    public static Board[] gernerateBoards(Board[] population){
+
+        Memory memory = new Memory();
+        Random rand = new Random();
+        int[] possibleValues = generateRemainingValues(memory.board);
+        //fill the empty spaces with a random value to create the initial population of boards
+        for(int k = 0; k < 20; k++){
+            Board tempBoard = new Board();
+            possibleValues = generateRemainingValues(memory.board);
+            for(int i = 0; i < 9; i++){
+                for(int j = 0; j < 9; j++){
+                    if(memory.board.board[i][j] == 0){
+                        int n = rand.nextInt(possibleValues.length);
+                        tempBoard.board[i][j] = possibleValues[n];
+                        possibleValues = removeIntArrayElem(possibleValues, n);
+                    }else{
+                        tempBoard.board[i][j] = memory.board.board[i][j];
+                    }
+                }
+            }
+            Board.printBoard(tempBoard.board);
+            population[k] = tempBoard;
+        }
+        return population;
     }
 
     public static Board mutate(Board parent){
