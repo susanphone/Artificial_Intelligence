0308553b43225704df2855eed359815943afc9f8
Susan McCartney <57429748+susanphone@users.noreply.github.com>
2021-09-20 12:07:57 -0600

    fix format

--- Projects/Project1/sudoku_solver/Sudoku_Solver.java
+++ Projects/Project1/sudoku_solver/Sudoku_Solver.java
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-package sudoku_solver;
-
-public class Sudoku_Solver {
-
-    public static void main(String[] args) throws Exception {
-//        Agent.backtrack();
-//        Agent.backtrackForwardCheck();
-//        Agent.backtrackArcConsistency();
-//        System.out.println(1/3*3);
-//        System.out.println(8/3*3);
-//        AgentTemp.genetic();
-        Agent.simulatedAnnealing();
-//        LocalSearchAgent.simulatedAnnealing();
-    }
-}
-
-=======
 package sudoku_solver;
 
 public class Sudoku_Solver {
@@ -35,4 +17,3 @@ public class Sudoku_Solver {
     }
 }
 
->>>>>>> a71731ac539098c949a7f3e67dee146151758096
