5d2580eebe81dbe31693c7f90b7b74f0b47e5402
hbmadsen <>
2021-09-20 21:00:03 -0600

    video updates

--- Projects/Project1/sudoku_solver/Sudoku_Solver.java
+++ Projects/Project1/sudoku_solver/Sudoku_Solver.java
@@ -3,13 +3,13 @@ package sudoku_solver;
 public class Sudoku_Solver {
 
     public static void main(String[] args) throws Exception {
-//        Agent.backtrack();
-//        Agent.backtrackForwardCheck();
+        Agent.backtrack();
+        Agent.backtrackForwardCheck();
 //        Agent.backtrackArcConsistency();
 //        System.out.println(1/3*3);
 //        System.out.println(8/3*3);
 //        AgentTemp.genetic();
-        Agent.simulatedAnnealing();
+//        Agent.simulatedAnnealing();
     }
 }
 
