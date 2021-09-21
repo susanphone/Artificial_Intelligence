9b09f241856a909e866368a3301331591d6b72bb
hbmadsen <>
2021-09-20 22:01:33 -0600

    finishing touches

--- Projects/Project1/sudoku_solver/Sudoku_Solver.java
+++ Projects/Project1/sudoku_solver/Sudoku_Solver.java
@@ -2,14 +2,13 @@ package sudoku_solver;
 
 public class Sudoku_Solver {
 
+    //uncomment to run desired algorithm
     public static void main(String[] args) throws Exception {
-        Agent.backtrack();
-        Agent.backtrackForwardCheck();
+//        Agent.backtrack();
+//        Agent.backtrackForwardCheck();
 //        Agent.backtrackArcConsistency();
-//        System.out.println(1/3*3);
-//        System.out.println(8/3*3);
-//        AgentTemp.genetic();
 //        Agent.simulatedAnnealing();
+//        GeneticAgent.genetic();
     }
 }
 
