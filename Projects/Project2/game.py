d2ca54c711053e9188cb9561e87a25fd4b37cef1
Susan McCartney <57429748+susanphone@users.noreply.github.com>
2021-10-04 11:35:20 -0600

    steps for project 2

--- Projects/Project2/game.py
+++ Projects/Project2/game.py
@@ -1,3 +1,9 @@
 from Project2 import Statistics, Logic, Cell, Board, Explorer
 
+"""1. Generate Board and Cells, probability based
+# 2. Build Logic - truth tables for ea cell
+# 3. Run Explorer
+    use Logic to solve the board
+    expands upon logic
+"""
 # this is where we will play the game
