4a839a796f6eb9af1e97e2d7cb1329d2cd252022
hbmadsen <>
2021-10-12 14:38:34 -0600

    updated neighbors function

--- Projects/Project2/wumpus_world/hannahLogic.py
+++ Projects/Project2/wumpus_world/hannahLogic.py
@@ -11,6 +11,8 @@ def get_neighbors(current_cell, board):
     current_row = current_cell.y
     current_column = current_cell.x
     neighbors = []
+    max_row = board[-1].y #the row for the last cell in the list
+    max_col = board[-1].x #the col for the last cell in the list
 
     for c in board:
         if c.x == current_row + 1 and c.y == current_column:
@@ -21,18 +23,29 @@ def get_neighbors(current_cell, board):
             west_state = c.state
         if c.x == current_row and c.y == current_column + 1:
             east_state = c.state
+    if current_row + 1 <= max_row:
+        north_neighbor = Cell(current_row + 1, current_column, north_state)
+        neighbors.append(north_neighbor)
+    else:
+        neighbors.append(None)
 
-    north_neighbor = Cell(current_row + 1, current_column, north_state)
-    neighbors.append(north_neighbor)
+    if current_row - 1 >= 0:
+        south_neighbor = Cell(current_row - 1, current_column, south_state)
+        neighbors.append(south_neighbor)
+    else:
+        neighbors.append(None)
 
-    south_neighbor = Cell(current_row - 1, current_column, south_state)
-    neighbors.append(south_neighbor)
+    if current_column - 1 >= 0:
+        west_neighbor = Cell(current_row, current_column - 1, west_state)
+        neighbors.append(west_neighbor)
+    else:
+        neighbors.append(None)
 
-    west_neighbor = Cell(current_row, current_column - 1, west_state)
-    neighbors.append(west_neighbor)
-
-    east_neighbor = Cell(current_row, current_column + 1, east_state)
-    neighbors.append(east_neighbor)
+    if current_column + 1 <= max_col:
+        east_neighbor = Cell(current_row, current_column + 1, east_state)
+        neighbors.append(east_neighbor)
+    else:
+        neighbors.append(None)
 
     return neighbors
 
