8cf33b90c09fc1b1264cbe4c8b8669c25c98885d
Austin Coop <90861276+acoop244@users.noreply.github.com>
2021-10-13 12:51:52 -0600

    Update Statistics.py

--- Projects/Project2/wumpus_world/Statistics.py
+++ Projects/Project2/wumpus_world/Statistics.py
@@ -4,32 +4,32 @@ class Statistics():
 
     def __init__(self):
         self.gold_found = 0
-		self.wumps_killed = 0
-		self.deaths_by_wumps = 0
-		self.deaths_by_pit = 0
-		self.cells_explored = 0
-		self.moves = 0
+	self.wumps_killed = 0
+	self.deaths_by_wumps = 0
+	self.deaths_by_pit = 0
+	self.cells_explored = 0
+	self.moves = 0
 
     def goldFound(self):
-		self.gold_found += 1
+	self.gold_found += 1
         pass
 
     def WumpusKilled(self):
-		self.wumps_killed += 1
+	self.wumps_killed += 1
         pass
 
     def deathByWumpus(self):
-		self.deaths_by_wumps += 1
+	self.deaths_by_wumps += 1
         pass
 
     def deathByPit(self):
-		self.deaths_by_pit += 1
+	self.deaths_by_pit += 1
         pass
 
     def cellsExplored(self):
-		self.cells_explored += 1
+	self.cells_explored += 1
         pass
 
-	def incrementMoves(self):
-		self.moves += 1
-		pass
+    def incrementMoves(self):
+	self.moves += 1
+	pass
