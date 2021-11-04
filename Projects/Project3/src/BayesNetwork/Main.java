014c89940fab505700848ab98073382c61420baf
Susan McCartney <57429748+susanphone@users.noreply.github.com>
2021-11-04 08:51:23 -0600

    main reader

--- Projects/Project3/src/BayesNetwork/Main.java
+++ Projects/Project3/src/BayesNetwork/Main.java
@@ -1,12 +1,11 @@
 package BayesNetwork;
 
+import org.w3c.dom.NodeList;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main {
 
@@ -20,10 +19,10 @@ public class Main {
 //        String d  = Arrays.toString(data);
 //        String[] str = d.split(" ");
 //        System.out.println(Arrays.toString(str));
-        TreeMap<String, ArrayList> dat = Reader.getVariables(data);
+        TreeMap<String, ArrayList> dat = Reader.getVariables(file);
         System.out.println(dat);
-        TreeMap<ArrayList, ArrayList> probs = Reader.getProbabilities(data);
-        System.out.println(probs);
+        TreeMap<ArrayList, ArrayList> probs = Reader.getProbabilities(file, dat);
+//        System.out.println(probs);
 
     }
 
