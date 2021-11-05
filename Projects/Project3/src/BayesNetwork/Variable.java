352b90667861d4ca2a2e75e13e9e2f39547aba91
hbmadsen <>
2021-11-05 11:51:27 -0600

    BayesNetwork & Variable

--- Projects/Project3/src/BayesNetwork/Variable.java
+++ Projects/Project3/src/BayesNetwork/Variable.java
@@ -1,13 +1,19 @@
 package BayesNetwork;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Variable {
-    public String states;
-    public HashMap probabilities;
+    public String name;
+    public String[] states;
+    public String[] parents;
+    public String[] children;
+    public HashMap<String, ArrayList> probabilities = new HashMap<String, ArrayList>();
 
-    public Variable(String s, HashMap prob){
+    public Variable(String name, String[] s, String[] p, String[] c){
         this.states = s;
-        this.probabilities = prob;
+        this.name = name;
+        this.parents = p;
+        this.children = c;
     }
 }
