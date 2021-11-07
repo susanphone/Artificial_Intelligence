e15fb3f42957c46fd98e150da7b393b8793009de
Susan McCartney <57429748+susanphone@users.noreply.github.com>
2021-11-07 15:28:18 -0700

    trying to create a hashmap from the values of the probabilities map

--- Projects/Project3/src/BayesNetwork/BayesNet.java
+++ Projects/Project3/src/BayesNetwork/BayesNet.java
@@ -1,8 +1,11 @@
 package BayesNetwork;
 
+import com.sun.jdi.Value;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -27,5 +30,21 @@ public class BayesNet {
 
     public String getNetworkName() {
         return networkName;
+    }
+    public HashMap setProbabilities(File file) throws FileNotFoundException {
+        HashMap<String, double[]> probabilitiesMap = new HashMap<>();
+        double [] probabilities = new double[0];
+        TreeMap vars = Reader.getVariables(file);
+        Map probs = Reader.getProbabilities(file, vars);
+        for (Object v: probs.values()) {
+            while (v) {
+                String state = v[0];
+                if (v.equals(type(double))) {
+                    probabilities = probabilities + v;
+                }
+                probabilitiesMap.put(state, probabilities);
+            }
+        }
+        return probabilitiesMap;
     }
 }
