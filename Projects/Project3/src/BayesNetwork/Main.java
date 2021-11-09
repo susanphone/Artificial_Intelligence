6ce39053cc724f0ee9e18b3ead2439363feef62a
hbmadsen <>
2021-11-09 11:06:30 -0700

    comments

--- Projects/Project3/src/BayesNetwork/Main.java
+++ Projects/Project3/src/BayesNetwork/Main.java
@@ -15,15 +15,23 @@ public class Main {
         data = Reader.cleanUpFile(file);
 
 
-        System.out.println(data);
-//        String d  = Arrays.toString(data);
-//        String[] str = d.split(" ");
-//        System.out.println(Arrays.toString(str));
-        TreeMap<String, ArrayList<String>> dat = Reader.getVariables(file);
-        System.out.println(dat);
-        Map<String, ArrayList<String>> probs = Reader.getProbabilities(file, dat);
-        System.out.println(probs);
+//        System.out.println(data);
+////        String d  = Arrays.toString(data);
+////        String[] str = d.split(" ");
+////        System.out.println(Arrays.toString(str));
+//        TreeMap<String, ArrayList<String>> dat = Reader.getVariables(file);
+//        System.out.println(dat);
+//        Map<String, ArrayList<String>> probs = Reader.getProbabilities(file, dat);
+//        System.out.println(probs);
 
+        HashMap<String, ArrayList> testMap = BayesNet.setProbabilities(file);
+        for (Map.Entry<String, ArrayList> item: testMap.entrySet()){
+            System.out.println("key");
+            System.out.println(item.getKey());
+            for(Object o: item.getValue()){
+                System.out.println(o);
+            }
+        }
         //below is test code for variable elimination on the earthquake network
         BayesNet earthquake = ExactTest.returnEarthquakeNet();
 
@@ -37,13 +45,13 @@ public class Main {
         evidenceStates.add("True");
 
         HashMap<String, ArrayList<Double>> evidenceFactors = Exact.variableElimination(earthquake, "Burglary", evidence, evidenceStates );
-        for (Map.Entry<String, ArrayList<Double>> item: evidenceFactors.entrySet()){
-            System.out.print(item.getKey() + " ");
-            for (double d : item.getValue()) {
-                System.out.print(d);
-            }
-            System.out.println("");
-        }
+//        for (Map.Entry<String, ArrayList<Double>> item: evidenceFactors.entrySet()){
+//            System.out.print(item.getKey() + " ");
+//            for (double d : item.getValue()) {
+//                System.out.print(d);
+//            }
+//            System.out.println("");
+//        }
     }
 
 }
