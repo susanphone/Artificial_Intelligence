6dbad66be684229345298c8ec2a316735ec06763
Susan McCartney <57429748+susanphone@users.noreply.github.com>
2021-11-09 08:49:19 -0700

    change reader and baysnet

--- Projects/Project3/src/BayesNetwork/Main.java
+++ Projects/Project3/src/BayesNetwork/Main.java
@@ -15,37 +15,38 @@ public class Main {
         data = Reader.cleanUpFile(file);
 
 
-        System.out.println(data);
-//        String d  = Arrays.toString(data);
-//        String[] str = d.split(" ");
-//        System.out.println(Arrays.toString(str));
-        TreeMap<String, ArrayList<String>> dat = Reader.getVariables(file);
+//        System.out.println(data);
+        TreeMap<String, ArrayList<String>> dat = Reader.getVariables(data);
         System.out.println(dat);
-        Map<String, ArrayList<String>> probs = Reader.getProbabilities(file, dat);
-        System.out.println(probs);
+        Map<String, ArrayList<String>> probs = Reader.getProbabilities(data);
+        System.out.println(probs.values());
+        Hashtable<String, Hashtable> p = new Hashtable<>();
+        p = BayesNet.setProbabilities(dat, probs);
+        System.out.println(p);
 
-        BayesNet earthquake = ExactTest.returnEarthquakeNet();
 
-        for (Variable v : earthquake.variables) {
-            System.out.println(v.name);
-            System.out.println("Parents: " + Arrays.toString(v.parents));
-            System.out.println("Children: " + Arrays.toString(v.children));
-            System.out.println("Probability Distribution: ");
-            for (Map.Entry<String, ArrayList<Double>> item: v.probabilities.entrySet()) {
-                System.out.print(item.getKey() + " ");
-                System.out.println(Arrays.toString(item.getValue().toArray()));
-            }
-            System.out.println("");
-        }
-        ArrayList<String> evidence = new ArrayList<>();
-        evidence.add("JohnCalls");
-        evidence.add("MaryCalls");
-
-        ArrayList<String> evidenceStates = new ArrayList<>();
-        evidenceStates.add("True");
-        evidenceStates.add("True");
-
-        HashMap<String, ArrayList<Double>> evidenceFactors = Exact.variableElimination(earthquake, "burglary", evidence, evidenceStates );
+//        BayesNet earthquake = ExactTest.returnEarthquakeNet();
+//
+//        for (Variable v : earthquake.variables) {
+//            System.out.println(v.name);
+//            System.out.println("Parents: " + Arrays.toString(v.parents));
+//            System.out.println("Children: " + Arrays.toString(v.children));
+//            System.out.println("Probability Distribution: ");
+//            for (Map.Entry<String, ArrayList<Double>> item: v.probabilities.entrySet()) {
+//                System.out.print(item.getKey() + " ");
+//                System.out.println(Arrays.toString(item.getValue().toArray()));
+//            }
+//            System.out.println("");
+//        }
+//        ArrayList<String> evidence = new ArrayList<>();
+//        evidence.add("JohnCalls");
+//        evidence.add("MaryCalls");
+//
+//        ArrayList<String> evidenceStates = new ArrayList<>();
+//        evidenceStates.add("True");
+//        evidenceStates.add("True");
+//
+//        HashMap<String, ArrayList<Double>> evidenceFactors = Exact.variableElimination(earthquake, "burglary", evidence, evidenceStates );
 
     }
 
