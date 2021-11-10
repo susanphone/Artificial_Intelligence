92d555bf902f3b76f475602d8eba5cf9b9502486
Susan McCartney <57429748+susanphone@users.noreply.github.com>
2021-11-09 17:21:58 -0700

    states and numbers get mixed

--- Projects/Project3/src/BayesNetwork/Reader2.java
+++ Projects/Project3/src/BayesNetwork/Reader2.java
@@ -3,6 +3,8 @@ package BayesNetwork;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.regex.*;
+
 
 public class Reader2 {
     public static ArrayList<String> cleanUpFile(File file) throws FileNotFoundException {
@@ -159,66 +161,68 @@ public class Reader2 {
                     pc.add(parents);
                     pc.toString();
                     System.out.println(pc);
-                    i++;
+                    i= i + 1;
                 }
-                    String state1 = null;
-                    String state2 = null;
-                    String state3 = null;
-                    String state4 = null;
-                    if (variable.containsValue(list.get(i))) {
-                        state1 = (String) list.get(i);
-                        states.add(state1);
+                String state1 = null;
+                String state2 = null;
+                String state3 = null;
+                String state4 = null;
+                if (!Objects.equals(list.get(i), "}")) {
+                    state1 = (String) list.get(i);
+                    states.add(state1);
+                    i = i + 1;
+                }
+                if (!Objects.equals(list.get(i), "}") && list.get(i) != "^[0-9].[0-9][0-9]") {
+                    state2 = (String) list.get(i);
+                    states.add(state2);
+                    i = i + 1;
+                }
+                if (Objects.equals(list.get(i), "[^A-Za-z]")) {
+                    state3 = (String) list.get(i);
+                    states.add(state3);
+                    i = i + 1;
+                }
+                if (Objects.equals(list.get(i), "[(^A-Za-z)]")) {
+                    state4 = (String) list.get(i);
+                    states.add(state4);
+                    i = i + 1;
+                }
+                if (!Objects.equals(list.get(i), "}") && !Objects.equals(list.get(i), "[^A-Za-z]")) {
+                    double p1 = 0.0;
+                    double p2 = 0.0;
+                    double p3 = 0.0;
+                    double p4 = 0.0;
+                    if (!Objects.equals(list.get(i), "}")) {
+                        p1 = (Double) list.get(i);
+                        numbers.add(p1);
                         i = i + 1;
                     }
-                    if (variable.containsValue(list.get(i))) {
-                        state2 = (String) list.get(i);
-                        states.add(state2);
+                    if (!Objects.equals(list.get(i), "}")) {
+                        p2 = (Double) list.get(i);
+                        numbers.add(p2);
                         i = i + 1;
                     }
-                    if (variable.containsValue(list.get(i))) {
-                        state3 = (String) list.get(i);
-                        states.add(state3);
+                    if (!Objects.equals(list.get(i), "}")) {
+                        p3 = (Double) list.get(i);
+                        numbers.add(p3);
                         i = i + 1;
                     }
-                    if (variable.containsValue(list.get(i))) {
-                        state4 = (String) list.get(i);
-                        states.add(state4);
-                    } if (Objects.equals(list.get(i), "\\d")) {
-                        double p1 = 0.0;
-                        double p2 = 0.0;
-                        double p3 = 0.0;
-                        double p4 = 0.0;
-                        if (!Objects.equals(list.get(i), "}")) {
-                            p1 = (Double) list.get(i);
-                            numbers.add(p1);
-                            i = i + 1;
-                        }
-                        if (!Objects.equals(list.get(i), "}")) {
-                            p2 = (Double) list.get(i);
-                            numbers.add(p2);
-                            i = i + 1;
-                        }
-                        if (!Objects.equals(list.get(i), "}")) {
-                            p3 = (Double) list.get(i);
-                            numbers.add(p3);
-                            i = i + 1;
-                        }
-                        if (!Objects.equals(list.get(i), "}")) {
-                            p4 = (Double) list.get(i);
-                            numbers.add(p4);
-                        }
-                        System.out.println(states);
-                        System.out.println(numbers);
-                        problem.put(states, numbers);
-                        states = new ArrayList<>();
-                        numbers = new ArrayList<>();
+                    if (!Objects.equals(list.get(i), "}")) {
+                        p4 = (Double) list.get(i);
+                        numbers.add(p4);
+                    }
+                    System.out.println(states);
+                    System.out.println(numbers);
+                    problem.put(states, numbers);
+                    states = new ArrayList<>();
+                    numbers = new ArrayList<>();
 
-                    }
+                }
                 probabilities.put(pc.toString(), problem);
                 pc = new ArrayList();
                 problem = new HashMap<>();
             }
         }
-            return probabilities;
-        }
+        return probabilities;
     }
+}
