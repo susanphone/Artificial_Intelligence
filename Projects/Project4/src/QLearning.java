95f4d4695b6362ee58e9fc9ce94731b227aa55d5
Susan McCartney <57429748+susanphone@users.noreply.github.com>
2021-12-02 12:00:55 -0700

    Qlearning algorithm setup

--- Projects/Project4/src/QLearning.java
+++ Projects/Project4/src/QLearning.java
@@ -1,5 +1,6 @@
 public class QLearning {
 
+    int[] actions;
     char state;
     int reward;
     double[] position;
@@ -9,13 +10,35 @@ public class QLearning {
         this.reward = r;
         this.position = p;
     }
-
-    public int costFunction(){
-        int t = 0;
-        return t;
+    private int[] getActions(){
+        return actions;
     }
 
-    public double[] decision(char state, int reward, double[] position){
+    public int costFunction(int moves){
+//        int t = 0;
+//        return t;
+        int updatedMoves = moves + 1;
+        return updatedMoves;
+    }
+
+    public double[] decision(int[] actions, char[] states, int reward, double[] position){
+        int r = states.length * actions.length;
+        double learningRatio = 0.1;
+        double discount = 0.99;
+        QLearning q = new QLearning(state, reward, position);
+        boolean converges = false;
+        while (!converges) {
+            for (char state : states) {
+                double currentMoveReward = 1.0;
+                // while state is not terminal, continue on track
+                while ((!state.equals("w") || state != "g") && currentMoveReward <= 0.01) {
+                    // max_0.1 Q(state, action)
+                    currentMoveReward -= 0.001;
+
+                }
+            }
+        }
+        // return q;
         return position;
     }
 
