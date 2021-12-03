4eeb4e3af6b7206c4785a14bf449240576ee77d3
Susan McCartney <57429748+susanphone@users.noreply.github.com>
2021-12-03 12:11:23 -0700

    qlearning car acceleration decision

--- Projects/Project4/src/QLearning.java
+++ Projects/Project4/src/QLearning.java
@@ -14,6 +14,7 @@ public class QLearning {
         return actions;
     }
 
+
     public int costFunction(int moves){
 //        int t = 0;
 //        return t;
@@ -21,25 +22,68 @@ public class QLearning {
         return updatedMoves;
     }
 
-    public double[] decision(int[] actions, char[] states, int reward, double[] position){
-        int r = states.length * actions.length;
+    public int decision(char[] states, int reward, double[] position){
+        int[] acts = getActions();
+        int action = 0;
+        int possibleReward = states.length * acts.length;
         double learningRatio = 0.1;
         double discount = 0.99;
-        QLearning q = new QLearning(state, reward, position);
+        QLearning q = new QLearning(this.state, reward, position);
         boolean converges = false;
         while (!converges) {
             for (char state : states) {
                 double currentMoveReward = 1.0;
                 // while state is not terminal, continue on track
-                while ((!state.equals("w") || state != "g") && currentMoveReward <= 0.01) {
+                while ((state != 'w' || state != 'g') && currentMoveReward <= 0.01) {
+
                     // max_0.1 Q(state, action)
                     currentMoveReward -= 0.001;
+                    double x = position[0];
+                    double y = position[1];
+                    for (int a : acts) {
+                        if (x + 1 != 'w' && y+1 == 'w'){
+                            // move east
+//                            Car.accelerate(1, 0);
+                            return a;
+                        }
+                        if (x + 1 == 'w' && y + 1 != 'w') {
+                            // move east, slight north
+//                            Car.accelerate(-1, 1);
+                            return a;
+                        }
+                        if (y + 1 != 'w' && x + 1 != 'w') {
+                            // move north-east
+//                            Car.accelerate(1, 1);
+                            return a;
+                        }
+                        if (y + 1 == 'w' && x - 1 != 'w') {
+                            // move west, slight south
+//                            Car.accelerate(1,-1);
+                            return a;
+                        }
+                        if (y-1 == 'w' && x - 1 == 'w') {
+                            // move south-west
+//                            Car.accelerate(-1, -1);
+                            return a;
+                        }
+                        if (y-1 != 'w' && x-1 == 'w') {
+                            // move north, slight west
+//                            Car.accelerate(-1, 1);
+                            return a;
+                        }
+                        if (y-1 != 'w' && x-1 != 'w') {
+                            // move north-west
+//                            Car.accelerate(1, 1);
+                            return a;
+                        }
 
+
+
+                    }
                 }
             }
         }
-        // return q;
-        return position;
+        return action;
     }
 
 }
