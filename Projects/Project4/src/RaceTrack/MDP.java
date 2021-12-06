dcf5ff137b102c7df5c4637d8b7167c4ee0ccbf3
hbmadsen <>
2021-12-06 13:51:19 -0700

    value iteration working

--- Projects/Project4/src/RaceTrack/MDP.java
+++ Projects/Project4/src/RaceTrack/MDP.java
@@ -5,20 +5,44 @@ import java.util.ArrayList;
 public class MDP {
 
     ArrayList<int[]> states = new ArrayList<>();
-    ArrayList<Integer> actions = new ArrayList<>();
-    ArrayList<Integer> discount = new ArrayList<>();
+    ArrayList<int[]> actions = new ArrayList<>();
+    ArrayList<int[]> finishPositions = new ArrayList<>();
+    ArrayList<int[]> wallPositions;
 
     public MDP(){
 
     }
 
-    public String transition(String state, String action){
+    public double transition(double accelProb, double nextStateValue, double nextStateValueAccelFails){
 
-        return state;
+        double transitionVal = (accelProb * nextStateValue) + ((1 - accelProb) * nextStateValueAccelFails);
+
+        return transitionVal;
     }
 
-    public int reward(String state, String action){
-        int temp = 0;
-        return temp;
+    public double reward(int[] state){
+
+        if(finishPositions.contains(state)){
+            return 0.0;
+        }else{
+            return -1;
+        }
     }
+
+    public ArrayList<int[]> setActions(){
+
+        ArrayList<int[]> newActions = new ArrayList<>();
+
+        for(int i = -1; i < 2; i++){
+            for(int j = -1; j < 2; j++){
+                //create an array list of all the possible acceleration actions
+                int[] action = new int[2];
+                action[0] = i; //acceleration in the x direction
+                action[1] = j;
+                newActions.add(action); //acceleration in the y direction
+            }
+        }
+        return newActions;
+    }
+
 }
--- Projects/Project4/src/RaceTrack/Main.java
+++ Projects/Project4/src/RaceTrack/Main.java
@@ -1,5 +1,8 @@
 package RaceTrack;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args){
 
@@ -12,5 +15,46 @@ public class Main {
             System.out.println();
         }
 
+        //initialize the possible velocities
+        ArrayList<int[]> velocities = new ArrayList<>();
+
+        for(int x = -5; x < 6; x++){
+            for(int y = -5; y < 6; y++){
+                int [] velocity = {x, y};
+                velocities.add(velocity);
+            }
+        }
+
+        //create a new Markov Decision Process
+        MDP currentTrack = new MDP();
+
+        //add the coordinates for the possible states to the states ArrayList in the MDP
+        for(int i = 0; i < track.length; i++){
+            for(int j = 0; j < track[i].length; j++){
+
+                //check if coordinates are a valid position (starting point, road, or finish line)
+                if (track[i][j] == 'S' || track[i][j] == 'R' || track[i][j] == 'F'){
+                    for(int[] v : velocities){
+                        int[] state = {j, i, v[0], v[1]};
+                        currentTrack.states.add(state);
+                    }
+                    if(track[i][j] == 'F'){
+                        int[] finishPos = {j, i};
+                        currentTrack.finishPositions.add(finishPos);
+                    }
+                }
+            }
+        }
+
+        //set-up the action possibilities
+        currentTrack.actions =  currentTrack.setActions();
+
+        //track dimensions
+        int[] trackDim = {track[0].length, track.length};
+
+        //train the value iteration algo
+
+        HashMap<int[], ActionValue> optimalPolicy = ValueIteration.decision(trackDim, currentTrack, 0.1, 0.1);
     }
+
 }
