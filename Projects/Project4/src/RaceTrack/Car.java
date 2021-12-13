fe0acba8f8887a4069be590a58cdbe720223596e
Austin Coop <90861276+acoop244@users.noreply.github.com>
2021-12-12 18:00:55 -0700

    Fixed the simulator and the car's acceleration, changed main to make a string hashmap

--- Projects/Project4/src/RaceTrack/Car.java
+++ Projects/Project4/src/RaceTrack/Car.java
@@ -13,10 +13,12 @@ public class Car {
     ArrayList<int[]> velocity_history = new ArrayList<>();
 
     public Car(int[] p, int[] v){
-        this.position = p;
-        this.velocity = v;
-        position_history.add(this.position);
-        velocity_history.add(this.position);
+        position = p;
+        velocity = v;
+        int[] temp1 = {p[0], p[1]};
+        position_history.add(temp1);
+        int[] temp2 = {v[0], v[1]};
+        velocity_history.add(temp2);
     }
 
     //Accelerates the car and updates velocity and position accordingly. Returns 0 for success, 1 for a crash, and 2 for a finish
@@ -27,25 +29,25 @@ public class Car {
         //Checks to see if the car successfully accelerates, then updates velocity and position if true
         if(new Random().nextDouble() < 0.8){
             //Updates velocity based on current acceleration
-            velocity[0] = x + velocity_history.get(velocity_history.size() - 1)[0];
-            velocity[1] = x + velocity_history.get(velocity_history.size() - 1)[1];
+            velocity[0] = x + velocity[0];
+            velocity[1] = x + velocity[1];
             
             //Checks to see if either the x or y velocity has gone over 5 or under -5. If so, velocity is set back to the max or min
             if(velocity[0] > 5 || velocity[0] < -5){
                 velocity[0] = 5 * (velocity[0]/Math.abs(velocity[0]));
             }
             if(velocity[1] > 5 || velocity[1] < -5){
-                velocity[2] = 5 * (velocity[1]/Math.abs(velocity[1]));
+                velocity[1] = 5 * (velocity[1]/Math.abs(velocity[1]));
             }
-        
             //Updates position based on new velocity
-            position[0] = velocity[0]+ position_history.get(position_history.size() - 1)[0];
-            position[1] = velocity[1] + position_history.get(position_history.size() - 1)[1];
+            position[0] = velocity[0]+ position[0];
+            position[1] = velocity[1] + position[1];
         }
-        
-        //Adds the velocity and position at the current timestep to the history list
-        velocity_history.add(velocity);
-
+        else{
+            //Updates position based on new velocity
+            position[0] = velocity[0]+ position[0];
+            position[1] = velocity[1] + position[1];
+        }
         
         double m  = ((double)(position[1] - last_y)) / ((double)(position[0] - last_x));
         double b = ((double)last_y) - (m * ((double)last_x));
@@ -96,22 +98,34 @@ public class Car {
         for(int i = 0; i < checks.size(); i++){
             int[] current = checks.get(i);
             if(track[current[0]][current[1]] == 'W'){
-                if(checks.size()>0){
+                if(checks.size()>0 && i > 0){
+                    System.out.println("hey");
                     position = checks.get(i-1);
-                    position_history.add(position);
                 }
                 else{
-                    position = position_history.get(position_history.size()-1);
-                    position_history.add(position);
+                    position[0] = position_history.get(position_history.size()-1)[0];
+                    position[1] = position_history.get(position_history.size()-1)[1];
                 }
+                
+                int[] p = {position[0], position[1]};
+                position_history.add(p);
+                int[] v = {0, 0};
+                velocity = v;
+                velocity_history.add(v);
+                
+                
                 return 1;
             }
             else if(track[current[0]][current[1]] == 'F'){
                 position_history.add(position);
+                velocity_history.add(velocity);
                 return 2;
             }
         }
-        position_history.add(position);
+        int[] p = {position[0], position[1]};
+        position_history.add(p);
+        int[] v = {velocity[0], velocity[1]};
+        velocity_history.add(v);
         return 0;
     }
 
--- Projects/Project4/src/RaceTrack/Main.java
+++ Projects/Project4/src/RaceTrack/Main.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
 
-        char[][] track = Reader.trackIn("O-track.txt");
+        char[][] track = Reader.trackIn("L-track.txt");
 
         for (int i = 0; i < track.length; i++) {
             for (int j = 0; j < track[i].length; j++) {
@@ -16,6 +16,8 @@ public class Main {
             }
             System.out.println();
         }
+        
+        /*
 
         //initialize the possible velocities
         ArrayList<int[]> velocities = new ArrayList<>();
@@ -51,7 +53,7 @@ public class Main {
         currentTrack.actions = currentTrack.setActions();
 
         //track dimensions
-        int[] trackDim = {track[0].length, track.length};
+        int[] trackDim = {track[0].length, track.length}; */
 
         /*
          * OUTPUT FROM VALUE ITERATION
@@ -64,7 +66,7 @@ public class Main {
          * Given the starting position you should be able to use my optimalPolicy to help the car decide what is the
          * optimal action the take (the action attribute in the ActionValue object)
          */
-
+/*
         HashMap<int[], ActionValue>  optimalPolicy = ValueIteration.decision(currentTrack, 0.00001, 0.7, 0);
         System.out.println(optimalPolicy.size());
 
@@ -83,63 +85,67 @@ public class Main {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+        
+        */
+        
+        
+        
+        
 
-//        HashMap<int[], int[]>  optimalPolicy = new HashMap<>();
-//
-//        try {
-//            File myObj = new File("VI-L-track-Policy.txt");
-//            Scanner myReader = new Scanner(myObj);
-//            while (myReader.hasNextLine()) {
-//                String data = myReader.nextLine();
-//                int[] fileData = new int[6];
-//                int i = 0;
-//
-//                for(String s: data.split(" ")){
-//                    //System.out.println("String: " + s);
-//                    int sInt = Integer.parseInt(s);
-//                    //System.out.println(sInt);
-//                    fileData[i] = sInt;
-//                    i++;
-//                }
-//
-//                int[] states = new int[4];
-//                int[] action = new int[2];
-//
-//                states[0] = fileData[0];
-//                states[1] = fileData[1];
-//                states[2] = fileData[2];
-//                states[3] = fileData[3];
-//
-//                action[0] = fileData[4];
-//                action[1] = fileData[5];
-//
-//                optimalPolicy.put(states, action);
-//
-//                if(states[0] == 1 && states[1] == 6 && states[2] == 0 && states[3] == 0){
-//                    System.out.println("X Action: " + action[0]);
-//                    System.out.println("Y Action: " + action[1]);
-//                }
-//
-//
-//            }
-//            myReader.close();
-//        } catch (FileNotFoundException e) {
-//            System.out.println("An error occurred.");
-//            e.printStackTrace();
-//        }
-//
-//        System.out.println(optimalPolicy.size());
+        HashMap<String, int[]>  optimalPolicy = new HashMap<>();
+
+        try {
+            File myObj = new File("VI-L-track-Policy.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                int[] fileData = new int[6];
+                int i = 0;
+
+                for(String s: data.split(" ")){
+                    //System.out.println("String: " + s);
+                    int sInt = Integer.parseInt(s);
+                    //System.out.println(sInt);
+                    fileData[i] = sInt;
+                    i++;
+                }
+
+                int[] states = new int[4];
+                int[] action = new int[2];
+
+                states[0] = fileData[0];
+                states[1] = fileData[1];
+                states[2] = fileData[2];
+                states[3] = fileData[3];
+
+                action[0] = fileData[4];
+                action[1] = fileData[5];
+
+                String temp = "";
+                temp = temp.concat(String.valueOf(states[0]) + " ");
+                temp = temp.concat(String.valueOf(states[1]) + " ");
+                temp = temp.concat(String.valueOf(states[2]) + " ");
+                temp = temp.concat(String.valueOf(states[3]));
+                
+                optimalPolicy.put(temp, action);
+
+                if(states[0] == 1 && states[1] == 6 && states[2] == 0 && states[3] == 0){
+                    System.out.println("X Action: " + action[0]);
+                    System.out.println("Y Action: " + action[1]);
+                }
 
 
-        /* QLearning starts with initializing a map of the knowledge, the states are all unknown
-         * and the action is set as zero, which means no change to speed. */
-//
-        HashMap<int[], HashMap<int[], int[]>> trackLearning = new HashMap<>();
-        QLearning ql = new QLearning(trackDim, 0.001, 1);
-        trackLearning = ql.initailizeQTable(trackDim, ql.states, ql.actions);
-        ql.explore(ql.position, trackLearning);
-        HashMap path = new HashMap();
-        ql.decision(trackLearning, 1, ql.position, path);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        //System.out.println(optimalPolicy.size());
+        
+        new Simulator(track, false, optimalPolicy);
+
     }
 
 }
